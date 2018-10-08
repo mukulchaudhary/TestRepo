c00f82088e23b116c03548c866e2df207c9bc020
Mukul Chaudhary <mchaudhary007@nam.ad.pwcinternal.com>
2018-10-08 23:40:34 +0530

    First commit

---
+++ NativeR.r
@@ -0,0 +1,19 @@
+print("========== package test1  ==========")
+print("========== package test2  ==========")
+#library("dplyr")
+#print("load package successfully")
+
+#print("========== args test ==========")
+#args = commandArgs(trailingOnly=TRUE)
+#print(paste("args:",args))
+
+#print("========== read file test ==========")
+#df <- read.table("Resource.txt",header = TRUE)
+#print(df)
+#print(R.version.string)
+
+x <- pi ^ (1:5)
+x
+print(x)
+
+
